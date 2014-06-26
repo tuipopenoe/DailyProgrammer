ab43fac63dc01732839ecf0a8531f8c4d34b0377
Tui Popenoe <tuipopenoe@gmail.com>
2014-06-25 23:26:38 -0500

    Challenge 167

---
+++ Challenge167.py
@@ -0,0 +1,34 @@
+# Tui Popenoe
+# Challenge 167
+
+def getInput():
+    print("Enter your paragraph: ")
+    paragraph = input()
+
+    return paragraph
+
+def createHTML():
+    f = open('index.html', 'w')
+
+    output ="""<!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title></title>
+                        </head>
+                        <body>
+                        </body>
+                            <p>""" + getInput() +
+            """             </p>
+                        </body>
+                    </html>"""
+    f.write(output)
+    f.close()
+    print(output)
+    return output
+
+def main():
+    createHTML()
+
+if __name__ == "__main__":
+    main()
+
