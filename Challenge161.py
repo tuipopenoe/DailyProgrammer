27f4ef10625e5cbe96ed640e299650d3225769dd
Tui Popenoe <tuipopenoe@gmail.com>
2014-06-27 23:54:54 -0500

    Challenge161.py

---
+++ Challenge161.py
@@ -0,0 +1,25 @@
+# Tui Popenoe
+# Challenge 161 Blackjack
+
+deck = [    '2S', '2C', '2D', '2H',
+            '3S', '3C', '3D', '3H',
+            '4S', '4C', '4D', '4H',
+            '5S', '5C', '5D', '5H',
+            '6S', '6C', '6D', '6H',
+            '7S', '7C', '7D', '7H',
+            '8S', '8C', '8D', '8H',
+            '9S', '9C', '9D', '9H',
+            '10S', '10C', '10D', '10H',
+            'JS', 'JC', 'JD', 'JH',
+            'QS', 'QC', 'QD', 'QH',
+            'KS', 'KC', 'KD', 'KH',
+            'AS', 'AC', 'AD', 'AH'
+            ]
+
+def dealHand():
+
+
+def shuffleDecks(n):
+
+
+def outputPercentage():
