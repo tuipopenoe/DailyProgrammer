80d647e90c3d2f736ab3fe02dba034a956b9a32a
Tui Popenoe <tuipopenoe@gmail.com>
2014-07-01 12:50:24 -0500

    Challenge156.py

---
+++ Challenge156.py
@@ -0,0 +1,20 @@
+# Tui Popenoe
+# Challenge 156 Simple Decoder
+
+""" Decodes an ASCII message where all the characters have added a 4 to the 
+    ASCII value."""
+
+encodedMessage ="""Etvmp$Jsspw%%%%[e}$xs$ks%$]sy$lezi$wspzih$xli$lmhhir$qiwweki2$Rs{$mx$mw$}syv$xyvr$xs$nsmrmr$sr$xlmw$tvero2$Hs$rsx$tswx$er}xlmrk$xlex${mpp$kmzi$e{e}$xlmw$qiwweki2$Pixtistpi$higshi$xli$qiwweki$sr$xlimv$s{r$erh$vieh$xlmw$qiwweki2$]sy$ger$tpe}$epsrkf}$RSX$tswxmrk$ls{$}sy$higshih$xlmw$qiwweki2$Mrwxieh$tswx$}syv$wspyxmsr$xs$fi$}syvjezsvmxi$Lipps${svph$tvskveq$mr$sri$perkyeki$sj$}syv$glsmgi2$Qeoi$wyvi$}syv$tvskveq$we}w$&Lipps$[svph%%%&${mxl$7$%$ex$xli$irh2$Xlmw${e}tistpi$fvs{wmrk$xli$gleppirki${mpp$xlmro${i$lezi$epp$pswx$syv$qmrhw2$Xlswi${ls$tswx$lipps{svph$wspyxmsrw${mxlsyx$xli$xlvii$%%%${mpp$lezi$rsx$higshih$xli$qiwweki$erh$ws$}sy$ger$tspmxip}$tsmrx$syx$xlimv$wspyxmsr$mw$mr$ivvsv$,xli}$evi$nywx$jspps{mrk$epsrk${mxlsyx$ors{mrk-Irns}$xlmw$jyr2$Xli$xvyxl${mpp$fi$liph$f}$xlswi${ls$ger$higshi$xli$qiwweki2$>-"""
+
+def decode(message):
+    output = list()
+    for char in message:
+        output.append(chr(ord(char) -4))
+
+    print(''.join(output))
+
+def main():
+    decode(encodedMessage)
+
+if __name__=='__main__':
+    main()
---
+++ Challenge157.py
@@ -0,0 +1,7 @@
+# Tui Popenoe
+# Challenge 157 The Winning Move X-Games edition
+
+""" Calculates if the next move in a game of Tic Tac Toe could be a winning 
+    move, given the next player's move and the current board."""
+
+def winningMove(grid):
