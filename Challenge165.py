c6ddce4ccdb40258150a45b442810b64ac389d4a
Tui Popenoe <tuipopenoe@gmail.com>
2014-07-01 01:09:46 -0500

    Conway's Game of Life

--- Challenge165.py
+++ Challenge165.py
@@ -1,47 +1,56 @@
 # Tui Popenoe
 # Challenge 165 Game of Life
 
-""" Implementation of a cellular replication simulation. """
+""" Implementation of a cellular replication simulation. Conway's game of life
+    http://en.wikipedia.org/wiki/Conways_Game_of_Life"""
 
 import sys
 
-def createGrid(x, y):
-    grid = [['.' for i in xrange(x)] for j in xrange(y)]
-    return grid
-
-def createGrid(filename):
-    f = open(filename, 'r')
-    inputGrid = f.readlines()
+def life(filename=None):
+    """ Open file and create a grid of cells. Iterate N times. """
     grid = list()
-    for line in inputGrid:
-        grid.append([])
-        for char in line:
-            grid[line].append(char)
+    if filename == None:
+        filename = input.txt
+    f = open(filename, 'r')
+    [N, X, Y] = [int(i) for i in f.readline().split()]
+    grid = f.read().split()
 
-def iterateCell(cellState='off', neighbors):
-    for i in neighbors:
-        if neighbors[i] == 'on':
-            count += 1
+    for n in range(N):
+        newGrid =''
+        for x in range(X):
+            for y in range(Y):
+                # Get adjacent neighbors
+                count = getAdjacent(grid, x, y, X, Y)
 
-    # cellState == off
-    if cellState == '.' and count == 3:
-        return '#'
-    # cellState == on
-    elif cellState == '#' and count <= 1:
-        return '.'
-    # cellState == on
-    elif cellState == '#' and count >= 4:
-        return '.'
+            if grid[x][y] == '.' and count == 3:
+                newGrid += '#'
+            elif grid[x][y] == '#' and count not in [2, 3]:
+                newGrid += '.'
+            else:
+                newGrid += grid[x][y]
+        newGrid += '\n'
+    grid = newGrid.split()
 
-    print('Error: Invalid state')
-    return '.'
+    printGrid(grid)
+
+def getAdjacent(grid, x, y, X, Y):
+    """ Get the adjacent cells for a cell located at grid[x][y] in a grid of 
+    size X by Y """
+    count = 0
+    # % X, % Y gets cells that are wrapped around the grid
+    # Get x coordinates for adjacent cells 
+    for i in [(x-1) % X, x, (x+1) % X]:
+        # Get y coordinates for adjacent cells
+        for j in [(y-1) % Y, y, (y+1) % Y]:
+            # if the cell is on and not the center of the looped cells
+            if (i, j) != (x, y) and grid[i][j] == '#':
+                count += 1
+    return count
 
 def printGrid(grid):
-    for line in grid:
-        print line
-
-def lifeLoop(grid):
-    count = 0
+    """Print the grid of cells"""
+    print('\n'.join(grid))
 
 def main():
-    
+    if sys.argv[1]:
+        life(sys.argv[1])
