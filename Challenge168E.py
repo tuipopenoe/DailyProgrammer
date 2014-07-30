e7273e37b4df113c5509a731d408e752dd1063a0
Tui Popenoe <tuipopenoe@gmail.com>
2014-07-30 14:09:28 -0500

    Unit Tests Challenge168E.py

--- Challenge168E.py
+++ Challenge168E.py
@@ -1,6 +1,7 @@
 # Tui Popenoe
 # Challenge 168 (E) String Index
 
+import argparse
 import re
 import sys
 import unittest
@@ -9,25 +10,26 @@ class StringIndex():
     """
     StringIndex class containing methods for searching word indices in a string
     """
-
-    def indexString(self, string):
+    def index_string(self, string):
         """ (str) -> list
         Returns a list of the words in a string
         """
         lst = ['']
-        lst = re.findall(r"[a-zA-Z0-9]+", string)
-        for i in lst:
-            print(i)
+        lst.extend(re.findall(r"\w+", string))
+        print(lst)
         return lst
 
 
-    def getWordAt(self, i, lst):
+    def get_word_at(self, i, lst):
         """ (int, list)
         Returns the item at index i in the list
         """
-        return lst[i]
+        if i >= len(lst) or i < 0:
+            return lst[0]
+        else:
+            return lst[i]
 
-    def getWords(self, lst, indices=None):
+    def get_words(self, lst, indices=None):
         """ (list) -> list
         Return a list of words for the given indices
         """
@@ -37,19 +39,65 @@ class StringIndex():
         else:
             indices = raw_input().split()
         for i in indices:
-            words.append(self.getWordAt(int(i), lst))
+            words.append(self.get_word_at(int(i), lst))
         return words
 
     def main(self):
         """ None -> None
         Print a string containing the indices of words in an input string
         """
-        self.words = self.indexString(sys.argv[1])
-        print(self.getWords(self.words))
+        self.words = self.index_string(sys.argv[1])
+        print(self.get_words(self.words))
 
 class TestStringIndex(unittest.TestCase):
+    """
+    Test class for testing StringIndex
+    """
+    def setUp(self):
+        """
+        Setup variables for the unit tests
+        """
+        self.testInput = '...You...!!!@!3124131212 Hello have this is a --- '\
+            'string Solved !!...? to test @\n\n\n#!#@#@%$**#$@ Congratz'\
+            ' this!!!!!!!!!!!!!!!!one ---Problem\n\n'
+        self.testIndices = [12, -1, 1, -100, 4, 1000, 9, -1000, 16, 13, 17, 15]
+        self.stringIndex = StringIndex()
 
+    def test_index_string(self):
+        """
+        Test StringIndex index_string method
+        """
+        words = ['', 'You', '3124131212', 'Hello', 'have', 'this', 'is', 'a', \
+            'string', 'Solved', 'to', 'test', 'Congratz', 'this', 'one', \
+            'Problem']
+        self.assertEqual(words, self.stringIndex.index_string(self.testInput))
+
+    def test_get_word_at(self):
+        """
+        Test StringIndex get_word_at method
+        """
+        words = ['', 'All', 'your', 'base', 'are', 'belong', 'to', 'us']
+        for i in range(len(words)):
+            self.assertEqual(words[i], 
+                self.stringIndex.get_word_at(int(i), words))
+        self.assertEqual(words[0], 
+            self.stringIndex.get_word_at(1000, words))
+
+    def test_get_words(self):
+        """
+        Test StringIndex get_words method
+        """
+        words = ['Congratz', '', 'You', '', 'have', '', 'Solved', \
+            '', '', 'this', '', 'Problem']
+        print('Words: ')
+        print(self.stringIndex.get_words(self.testInput,
+            self.testIndices))
+        self.assertEqual(words, self.stringIndex.get_words(
+            self.stringIndex.index_string(self.testInput),
+            self.testIndices))
 
 if __name__=='__main__':
-    main()
-
+    if len(sys.argv) == 1:
+        unittest.main()
+    else:
+        StringIndex.main()
