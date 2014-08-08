0111b693e4ce046068cd68edc168f4725b6668f1
Tui Popenoe <tuipopenoe@gmail.com>
2014-08-07 22:58:03 -0500

    Challenge80E.py - Anagrams

---
+++ Challenge80E/Challenge80E.py
@@ -0,0 +1,37 @@
+# Tui Popenoe
+# Challenge80E.py - Anagrams
+
+import sys
+
+def find_anagrams(word, dictionary_file):
+    output = list()
+    with open(dictionary_file, 'r') as f:
+        count = 0
+        dictionary = f.read().splitlines()
+        for element in dictionary:
+            if len(word) == len(element):
+                for i in word:
+                    if i in element:
+                        count += 1
+                if len(word) == count:
+                    output.append(element)
+                else:
+                    pass
+                count = 0
+            else:
+                pass
+    return output
+
+def main():
+    if len(sys.argv) == 2:
+        print('\n'.join(find_anagrams(sys.argv[1], 'english_dictionary.txt')))
+    elif len(sys.argv) > 2:
+        print('\n'.join(find_anagrams(sys.argv[1], sys.argv[1])))
+    else:
+        print('Invalid arguments: search for a word in an english dictionary'\
+            'by default, or a word in a dictionary file')
+        print('word')
+        print('word dictionary_file')
+
+if __name__=='__main__':
+    main()
