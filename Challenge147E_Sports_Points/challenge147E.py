d4346c6654163749ce6fa8c17f083d07a98ab014
Tui Popenoe <tuipopenoe@gmail.com>
2014-12-22 22:58:57 -0600

    Challenge147E Sports Scores

--- Challenge147E_Sports_Points/challenge147E.py
+++ Challenge147E_Sports_Points/challenge147E.py
@@ -1,3 +1,21 @@
 #!python2
 # Tui Popenoe
 # challenge147E - Sports Points
+
+import sys
+
+def valid_score(score):
+    invalid_scores = [1,2,4,5]
+    if score not in invalid_scores:
+        return True
+    else:
+        return False
+
+def main():
+    if len(sys.argv) > 1:
+        print(valid_score(int(sys.argv[1])))
+    else:
+        print(valid_score(int(raw_input('Enter a score to validate: '))))
+
+if __name__ == '__main__':
+    main()
