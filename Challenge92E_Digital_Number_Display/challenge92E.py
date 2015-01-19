17a413612fdbc1b8ec6477c196607d93b9ed07dc
Tui Popenoe <tuipopenoe@gmail.com>
2015-01-18 22:23:08 -0600

    Challenge 92E Digital Display

--- Challenge92E_Digital_Number_Display/challenge92E.py
+++ Challenge92E_Digital_Number_Display/challenge92E.py
@@ -2,11 +2,99 @@
 # Tui Popenoe
 # challenge92E.py - Digital Number Display
 
-def digital_display(number):
+import sys
 
+digital_numbers = {
+    '1': [  ['    + '],
+            ['    | '],
+            ['    | '],
+            ['    + '],
+            ['    | '],
+            ['    | '],
+            ['    + ']],
+    '2': [  [' +--+ '],
+            ['    | '],
+            ['    | '],
+            [' +--+ '],
+            [' |    '],
+            [' |    '],
+            [' +--+ ']],
+    '3': [  [' +--+ '],
+            ['    | '],
+            ['    | '],
+            [' +--+ '],
+            ['    | '],
+            ['    | '],
+            [' +--+ ']],
+    '4': [  [' +  + '],
+            [' |  | '],
+            [' |  | '],
+            [' +--+ '],
+            ['    | '],
+            ['    | '],
+            ['    + ']],
+    '5': [  [' +--+ '],
+            [' |    '],
+            [' |    '],
+            [' +--+ '],
+            ['    | '],
+            ['    | '],
+            [' +--+ ']],
+    '6': [  [' +--+ '],
+            [' |    '],
+            [' |    '],
+            [' +--+ '],
+            [' |  | '],
+            [' |  | '],
+            [' +--+ ']],
+    '7': [  [' +--+ '],
+            ['    | '],
+            ['    | '],
+            ['    + '],
+            ['    | '],
+            ['    | '],
+            ['    + ']],
+    '8': [  [' +--+ '],
+            [' |  | '],
+            [' |  | '],
+            [' +--+ '],
+            [' |  | '],
+            [' |  | '],
+            [' +--+ ']],
+    '9': [  [' +--+ '],
+            [' |  | '],
+            [' |  | '],
+            [' +--+ '],
+            ['    | '],
+            ['    | '],
+            [' +--+ ']],
+    '0': [  [' +--+ '],
+            [' |  | '],
+            [' |  | '],
+            [' +  + '],
+            [' |  | '],
+            [' |  | '],
+            [' +--+ ']]
+}
+
+def digital_display(number):
+    numbers = []
+    for i in number:
+        numbers.append(digital_numbers[i])
+    lines = []
+    for j in range(7):
+        line_out = []
+        for i in numbers:
+            line_out.append(i[j])
+        lines.append(line_out)
+    for i in lines:
+        print(i)
 
 def main():
-
+    if len(sys.argv) > 1:
+        print(digital_display(sys.argv[1]))
+    else:
+        print(digital_display(sys.stdin.readline()))
 
 if __name__ == '__main__':
     main()
