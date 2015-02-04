ee9daff056064b3e8af3c1f3daf814d04d0552f0
Tui Popenoe <tuipopenoe@gmail.com>
2015-02-03 23:18:43 -0600

    Challenge200E.py Flood Fill

--- Challenge200E_Flood_Fill/challenge200.py
+++ Challenge200E_Flood_Fill/challenge200.py
@@ -2,12 +2,36 @@
 # Tui Popenoe
 # challenge200.py - Flood Fill
 
-def flood_fill():
-    wh = sys.stdin.readline().split()
-    width = int(wh[0])
-    height = int(wh[1])
-    grid = [['.' for i in range(width)] for j in range(height)]
-    input_grid = []
-    for i in height:
-        input_grid.append(sys.stdin.readline())
-    xyc = sys.stdin.readline().split()
+import sys
+
+def flood_fill(input_file):
+    with open(input_file, 'r') as f:
+        wh = f.readline().split()
+        width = int(wh[0])
+        height = int(wh[1])
+        grid = [['.' for i in range(width)] for j in range(height)]
+        input_grid = []
+        for i in height:
+            input_grid.append(f.readline())
+        xyc = f.readline().split()
+        x = int(xyc[0])
+        y = int(xyc[1])
+        c = xyc[2]
+
+        for i in range(y):
+            for j in range(x):
+                if grid[j][i] == '.':
+                    grid[j][i] == c
+                else:
+                    break
+        return grid
+
+def main():
+    if len(sys.argv) > 1:
+        flood_fill(sys.argv[1])
+    else:
+        flood_fill(sys.stdin.readline())
+
+
+if __name__ == '__main__':
+    main()
