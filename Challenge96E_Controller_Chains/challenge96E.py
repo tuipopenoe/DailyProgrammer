48857b5a21cfd0406a380c2ea07b7abe788e7c8c
Tui Popenoe <tuipopenoe@gmail.com>
2015-02-16 22:57:13 -0600

    Challenge96E Controller Chains

--- Challenge96E_Controller_Chains/challenge96E.py
+++ Challenge96E_Controller_Chains/challenge96E.py
@@ -2,11 +2,20 @@
 # Tui Popenoe
 # challenge96E.py - Controller Chains
 
-def controller_chains():
+import sys
 
+def controller_chains(d):
+    ports = 2
+    while ports < (d / 20 +1):
+        ports += 3
+        d -= 12
+    return d / 20 + 1;
 
 def main():
-
+    if len(sys.argv) > 1:
+        print(controller_chains(int(sys.argv[1])))
+    else:
+        print(controller_chains(int(sys.stdin.readline())))
 
 if __name__ == '__main__':
     main()
