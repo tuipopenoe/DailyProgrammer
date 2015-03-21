c3c4778fca0d5532fa40629420665c022164e11b
Tui Popenoe <tuipopenoe@gmail.com>
2015-03-21 12:10:35 -0500

    Challenge206E.py fixed recurrence relation

--- Challenge206E_Recurrence_Relations/challenge206E.py
+++ Challenge206E_Recurrence_Relations/challenge206E.py
@@ -33,21 +33,30 @@ def eval_(node):
 
 def recurrence(n, expr):
     """Return the literal eval of the given expr and n"""
-    return eval_expr('%s%s' % (str(n), expr))
+    # print('Evaluating: %s%s' % (str(n), expr))
+    expr = expr.split()
+    n = str(n)
+    for i in expr:
+        n = str(eval_expr('%s%s' % (n, i)))
+        # print(n)
+    # print('= %s' % n)
+    return n
 
-def get_nth_term(expr, first_term, n):
-    if n == 0:
+def get_nth_term(expr, first_term, n, count):
+    print('Term %s: %s' % (n, recurrence(first_term, expr)))
+    if n == count:
         return first_term
     else:
-        return get_nth_term(expr, recurrence(first_term), n - 1)
+        return get_nth_term(expr, recurrence(first_term, expr), n + 1, count)
 
 def main():
     if len(sys.argv) > 1:
-        print(get_nth_term(sys.argv[1], sys.argv[2], sys.argv[3]))
+        print(get_nth_term(sys.argv[1], sys.argv[2], sys.argv[3]), sys.argv[3])
     else:
-        print(get_nth_term(stdin.readline(),
-                           stdin.readline(),
-                           stdin.readline()))
+        print(get_nth_term(sys.stdin.readline(),
+                           int(sys.stdin.readline()),
+                           0,
+                           int(sys.stdin.readline())))
 
 if __name__ == '__main__':
     main()
