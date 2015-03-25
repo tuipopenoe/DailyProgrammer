57051d97595d2ed030aeacff3fcf7cd5aa6df5b4
Tui Popenoe <tuipopenoe@gmail.com>
2015-03-25 11:53:19 -0500

    Pep8 fixes

--- Challenge207E_DNA_Replication/challenge207E.py
+++ Challenge207E_DNA_Replication/challenge207E.py
@@ -4,86 +4,90 @@
 
 import sys
 
+
 def get_dna_pairs():
     return {
-        'A' : 'T',
-        'T' : 'A',
-        'G' : 'C',
-        'C' : 'G'
+        'A': 'T',
+        'T': 'A',
+        'G': 'C',
+        'C': 'G'
     }
+
 
 def get_codons():
     return {
-        'TTT' : 'Phe',
-        'TTC' : 'Phe',
-        'TTA' : 'Leu',
-        'TTG' : 'Leu',
-        'CTT' : 'Leu',
-        'CTC' : 'Leu',
-        'CTA' : 'Leu',
-        'CTG' : 'Leu',
-        'ATT' : 'Ile',
-        'ATC' : 'Ile',
-        'ATA' : 'Ile',
-        'ATG' : 'Met',
-        'GTT' : 'Val',
-        'GTC' : 'Val',
-        'GTA' : 'Val',
-        'GTG' : 'Val',
-        'TCT' : 'Ser',
-        'TCC' : 'Ser',
-        'TCA' : 'Ser',
-        'TCG' : 'Ser',
-        'AGT' : 'Ser',
-        'AGC' : 'Ser',
-        'CCT' : 'Pro',
-        'CCC' : 'Pro',
-        'CCA' : 'Pro', 
-        'CCG' : 'Pro',
-        'ACT' : 'Thr',
-        'ACC' : 'Thr',
-        'ACA' : 'Thr',
-        'ACG' : 'Thr',
-        'GCT' : 'Ala',
-        'GCC' : 'Ala',
-        'GCA' : 'Ala',
-        'GCG' : 'Ala',
-        'TAT' : 'Tyr',
-        'TAC' : 'Tyr',
-        'TAA' : 'STOP',
-        'TAG' : 'STOP',
-        'CAT' : 'His',
-        'CAC' : 'His',
-        'CAA' : 'Gin',
-        'CAG' : 'Gin',
-        'AAT' : 'Asn',
-        'AAC' : 'Asn',
-        'AAA' : 'Lys',
-        'AAG' : 'Lys',
-        'GAT' : 'Asp',
-        'GAC' : 'Asp',
-        'GAA' : 'Glu',
-        'GAG' : 'Glu',
-        'TGT' : 'Cys',
-        'TGC' : 'Cys',
-        'TGA' : 'STOP',
-        'TGG' : 'Trp',
-        'CGT' : 'Arg',
-        'CGC' : 'Arg',
-        'CGA' : 'Arg',
-        'CGG' : 'Arg',
-        'AGA' : 'Arg',
-        'AGG' : 'Arg',
-        'GGT' : 'Gly',
-        'GGC' : 'Gly',
-        'GGA' : 'Gly',
-        'GGG' : 'Gly'
+        'TTT': 'Phe',
+        'TTC': 'Phe',
+        'TTA': 'Leu',
+        'TTG': 'Leu',
+        'CTT': 'Leu',
+        'CTC': 'Leu',
+        'CTA': 'Leu',
+        'CTG': 'Leu',
+        'ATT': 'Ile',
+        'ATC': 'Ile',
+        'ATA': 'Ile',
+        'ATG': 'Met',
+        'GTT': 'Val',
+        'GTC': 'Val',
+        'GTA': 'Val',
+        'GTG': 'Val',
+        'TCT': 'Ser',
+        'TCC': 'Ser',
+        'TCA': 'Ser',
+        'TCG': 'Ser',
+        'AGT': 'Ser',
+        'AGC': 'Ser',
+        'CCT': 'Pro',
+        'CCC': 'Pro',
+        'CCA': 'Pro',
+        'CCG': 'Pro',
+        'ACT': 'Thr',
+        'ACC': 'Thr',
+        'ACA': 'Thr',
+        'ACG': 'Thr',
+        'GCT': 'Ala',
+        'GCC': 'Ala',
+        'GCA': 'Ala',
+        'GCG': 'Ala',
+        'TAT': 'Tyr',
+        'TAC': 'Tyr',
+        'TAA': 'STOP',
+        'TAG': 'STOP',
+        'CAT': 'His',
+        'CAC': 'His',
+        'CAA': 'Gin',
+        'CAG': 'Gin',
+        'AAT': 'Asn',
+        'AAC': 'Asn',
+        'AAA': 'Lys',
+        'AAG': 'Lys',
+        'GAT': 'Asp',
+        'GAC': 'Asp',
+        'GAA': 'Glu',
+        'GAG': 'Glu',
+        'TGT': 'Cys',
+        'TGC': 'Cys',
+        'TGA': 'STOP',
+        'TGG': 'Trp',
+        'CGT': 'Arg',
+        'CGC': 'Arg',
+        'CGA': 'Arg',
+        'CGG': 'Arg',
+        'AGA': 'Arg',
+        'AGG': 'Arg',
+        'GGT': 'Gly',
+        'GGC': 'Gly',
+        'GGA': 'Gly',
+        'GGG': 'Gly'
     }
+
 
 def dna_replication(dna):
     print(' '.join(dna))
     dna_pairs = get_dna_pairs()
     return [dna_pairs[x] for x in dna]
+
 
 def codon_replication(dna):
     codons = get_codons()
@@ -98,6 +102,7 @@ def codon_replication(dna):
                 break
     return output
 
+
 def main():
     if len(sys.argv) > 1:
         print(' '.join(dna_replication(sys.argv[1:])))
@@ -107,6 +112,6 @@ def main():
         print(' '.join(dna_replication(dna_input)))
         print(' '.join(codon_replication(dna_input)))
 
+
 if __name__ == '__main__':
     main()
-
