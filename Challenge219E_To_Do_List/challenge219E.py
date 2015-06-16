b9d4cc3545124fa106cfbcd31d5d99571a1b29cf
Tui Popenoe <tuipopenoe@gmail.com>
2015-06-15 23:38:02 -0500

    Challenge219E.py

---
+++ Challenge219E_To_Do_List/challenge219E.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# Tui Popenoe
+# Challenge219E.py To Do List
+
+
+class ToDoList(object):
+	def __init__(self):
+		self.to_do_list = []
+
+	def add_item(item):
+		self.to_do_list.append(item)
+
+	def remove_item(item):
+		try:
+			self.to_do_list.remove(item)
+		except Exception, ex:
+			pass
+
+	def view_list():
+		for item in self.to_do_list:
+			print(item)
+
